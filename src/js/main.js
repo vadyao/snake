cda3223d223c478b9e121c215d8ebfc86ef531d7
vadyao <45079184+vadyao@users.noreply.github.com>
2018-11-27 20:04:48 +0200

    second setup button

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 var x = 1
+var y = 1
 
 //button listener
 window.addEventListener('keydown', (event) => {
@@ -8,5 +9,26 @@ window.addEventListener('keydown', (event) => {
         x++
         console.log('right', x * 24)
     }
+
     head.style.left = x * 24 + 'px'
+
+    if (event.key == 'ArrowLeft') {
+        x--
+        console.log('left', x * 24)
+    }
+    head.style.left = x * 24 + 'px'
+
+    if (event.key == 'ArrowUp') {
+        y--
+        console.log('up', y * 24)
+    }
+    head.style.top = y * 24 + 'px'
+
+    if (event.key == 'ArrowDown') {
+        y++
+        console.log('down', y * 24)
+    }
+    head.style.top = y * 24 + 'px'
+
+
 })
