32d70534ef7b92f7b10562984dfc665337cdb6d0
vadyao <45079184+vadyao@users.noreply.github.com>
2018-11-28 23:33:51 +0200

    snake loop

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,33 @@
-var x = 1
-var y = 1
+var x = 0
+var y = 0
 
 //button listener
-window.addEventListener('keydown', (event) => {
+window.addEventListener('keydown', gameLoop)
+
+function gameLoop(event) {
     const head = document.querySelector('.snake-head')
-    console.log(event.key)
-    if (event.key == 'ArrowRight') {
-        x++
-        console.log('right', x * 24)
-    }
+    const body = document.querySelector('.snake-body')
+    console.log(event)
 
+    body.style.top = y * 24 + 'px'
+    body.style.left = x * 24 + 'px'
+
+    if (event.key == 'ArrowRight') x++
+    if (event.key == 'ArrowLeft') x--
+    if (event.key == 'ArrowUp') y--
+    if (event.key == 'ArrowDown') y++
+
+    if (x > 9) x = 0
+    if (x < 0) x = 9
+    if (y > 9) y = 0
+    if (y < 0) y = 9
+
+    head.style.top = y * 24 + 'px'
     head.style.left = x * 24 + 'px'
 
-    if (event.key == 'ArrowLeft') {
-        x--
-        console.log('left', x * 24)
-    }
-    head.style.left = x * 24 + 'px'
 
-    if (event.key == 'ArrowUp') {
-        y--
-        console.log('up', y * 24)
-    }
-    head.style.top = y * 24 + 'px'
+}
 
-    if (event.key == 'ArrowDown') {
-        y++
-        console.log('down', y * 24)
-    }
-    head.style.top = y * 24 + 'px'
+gameLoop({ key: 0 })
 
 
-})
